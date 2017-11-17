e53b3a27109ab2a3f1d0d44a985366c991006ce6
nlsdfnbch <23okrs20+github@mykolab.com>
2017-11-17 09:53:13 +0100

    added long description

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-VERSION = '1.0'
+VERSION = '1.0.1'
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+long_description = f.read()
 
 setup(name='hermes-zmq',
       version=VERSION,
       description='ZMQ-based framework for building simple Pub-Sub Systems, written in Python 3.',
+      long_description=long_description,
       author='Nils Diefenbach',
       author_email='23okrs20+github@mykolab.com',
       packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
