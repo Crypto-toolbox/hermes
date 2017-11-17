c1630231ae0738a5a38a41734f357ec91506eb7a
nlsdfnbch <23okrs20+github@mykolab.com>
2017-11-17 23:28:17 +0100

    remove unavailable attr test

--- travis/execute_tests.sh
+++ travis/execute_tests.sh
@@ -2,10 +2,10 @@
 pip install coverage;
 python setup.py install
 cd tests;
-coverage run -p --source=hermes structs_tests.py test;
-coverage run -p --source=hermes reciever_tests.py test;
-coverage run -p --source=hermes publisher_tests.py test;
-coverage run -p --source=hermes node_tests.py test;
-coverage run -p --source=hermes proxy_tests.py test;
+coverage run -p --source=hermes structs_tests.py;
+coverage run -p --source=hermes reciever_tests.py;
+coverage run -p --source=hermes publisher_tests.py;
+coverage run -p --source=hermes node_tests.py;
+coverage run -p --source=hermes proxy_tests.py;
 coverage combine;
 coveralls;
