cb702a02cd1a60b048f1d9cf32f7bfa3e832f947
nlsdfnbch <23okrs20+github@mykolab.com>
2017-11-17 23:44:17 +0100

    added tests folder to setup.py

--- travis/execute_tests.sh
+++ travis/execute_tests.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 pip install coverage;
-python setup.py install
-cd tests;
-coverage run -p --source=hermes structs_tests.py;
-coverage run -p --source=hermes reciever_tests.py;
-coverage run -p --source=hermes publisher_tests.py;
-coverage run -p --source=hermes node_tests.py;
-coverage run -p --source=hermes proxy_tests.py;
-coverage combine;
+coverage run -p --source=hermes setup.py test;
 coveralls;
