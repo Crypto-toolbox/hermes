0ff547f8363a4e5da45396b0a917e5887b3db9e2
nlsdfnbch <23okrs20+github@mykolab.com>
2017-11-17 09:48:32 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 VERSION = '1.0'
 
-setup(name='hermes',
+setup(name='hermes-zmq',
       version=VERSION,
-      description='ZMQ-based framework for building Pub-Sub Systems, written in Python 3.',
+      description='ZMQ-based framework for building simple Pub-Sub Systems, written in Python 3.',
       author='Nils Diefenbach',
-      author_email='23okrs20+gitlab@mykolab.com',
-      packages=['hermes'],
+      author_email='23okrs20+github@mykolab.com',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
       classifiers=['Development Status :: 3 - Alpha',
                    'Intended Audience :: Financial and Insurance Industry',
                    'License :: Other/Proprietary License',
                    'Operating System :: POSIX :: Linux',
                    'Programming Language :: Python :: 3 :: Only',
                    'Topic :: Office/Business :: Financial :: Investment'],
-      package_data={'': ['*.md', '*.rst']})
+      package_data={'': ['*.md', '*.rst']},
+      keywords="zmq pubsub ipc distributed messaging", python_requires=">=3.5")
 
